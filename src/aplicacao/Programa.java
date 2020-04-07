297327615da97d21da7181c33579cc8094a6f597
Gabriel Silvestre <gabrielsilvestre3993@gmail.com>
2020-04-07 19:27:37 -0300

    Comecando a implementar tabuleiro e peca

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,14 @@
 package aplicacao;
 
 import jogotabuleiro.Posicao;
+import jogotabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
-		System.out.println(posicao);
 
 	}
 
