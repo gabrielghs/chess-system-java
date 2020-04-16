dd31e01dd0235c6c08e9b84c5834e2c723818dd2
Gabriel Silvestre <gabrielsilvestre3993@gmail.com>
2020-04-16 19:50:46 -0300

    Implementando movimentos possiveis do rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogotabuleiro.Posicao;
 import jogotabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -14,10 +15,60 @@ public class Rei extends PecaXadrez{
 	public String toString() {
 		return "R";
 	}
+	
+	private boolean podeMover(Posicao posicao) {
+		PecaXadrez p = (PecaXadrez)getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
+		
+	}
 
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao(0, 0); 
+			
+		//Acima
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna());
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Canto Superior Esquerdo
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Esquerda
+		p.definirValores(posicao.getLinha(), posicao.getColuna() - 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Canto Inferior Esquerdo
+		p.definirValores(posicao.getLinha()  + 1, posicao.getColuna() - 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Canto Superior Direito
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Direita
+		p.definirValores(posicao.getLinha(), posicao.getColuna() + 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Canto Inferior Direito
+		p.definirValores(posicao.getLinha()  - 1, posicao.getColuna() + 1);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		//Baixo
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna());
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
 		return mat;
 	}
 
